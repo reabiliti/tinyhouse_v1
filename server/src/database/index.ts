d2e8e03f63f95ec6902f373c003588bbdeda7c23
Alexander Dyuzhikov <alex.duzh@gmail.com>
2020-02-12 22:09:34 +0300

    Backend. Add generics for database.

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,10 +1,12 @@
 import { MongoClient } from 'mongodb'
 
+import { Database } from '../lib/types'
+
 const { DB_USER, DB_PASS, DB_CLUSTER } = process.env
 
 const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { useNewUrlParser: true })
 
   const db = client.db('main')
@@ -13,5 +15,3 @@ export const connectDatabase = async () => {
     listings: db.collection('test_listings')
   }
 }
-
-
